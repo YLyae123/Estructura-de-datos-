9db5994f38d9ed54be42c57bf56ebb7b5a90928c
YLyae123 <144478187+YLyae123@users.noreply.github.com>
2023-09-24 23:12:54 -0600

    Add files via upload

---
+++ mona lisa.cpp
@@ -0,0 +1,10 @@
+// Yael Aceituno
+
+int numeros[6] = {10, 20, 30, 40, 50, 60};
+
+int n[3] = {3, 4, 5}; // Declara un array de 3 elementos
+
+char c[4] = {'L', 'u', 'i', 's'}; // Declara un array de 4 elementos
+
+char s[] = "Mona Lisa"; // Declara un array de caracteres (cadena)
+
